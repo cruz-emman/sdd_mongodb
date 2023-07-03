e72ae5b844e6b15b9fdbc1c4751afb221b65f8bc
cruz-emman <113968577+cruz-emman@users.noreply.github.com>
2023-07-03 14:58:18 +0800

    Update Sidebar.jsx

--- server/src/components/Sidebar.jsx
+++ server/src/components/Sidebar.jsx
@@ -9,14 +9,14 @@ import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../assets/logo.png'
 import { logOut } from '../redux/adminSlice';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const Sidebar = () => {
 
 
   const {admin, isFetching, isError, isSuccess, isMessage} = useSelector((state) => state.admin)
   const techAdminType = admin && admin.affiliation;
-  //console.log(techAdminType)
+  console.log(techAdminType)
 
   const navigate = useNavigate()
   const handleLogoUt =  (e) =>{
